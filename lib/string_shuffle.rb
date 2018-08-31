eec4e2c673181488934357d40f76e22c980e13f8
OBaddeley <olivia.baddeley@gmail.com>
2018-09-01 10:20:46 +1200

    Add remove spaces method.

--- lib/string_shuffle.rb
+++ lib/string_shuffle.rb
@@ -6,4 +6,8 @@ module StringShuffle
     string.chars.shuffle.join
   end
 
+  def self.shuffle_remove_spaces(string)
+    shuffle(string).gsub(/\s+/, '')
+  end
+
 end
--- spec/string_shuffle_spec.rb
+++ spec/string_shuffle_spec.rb
@@ -5,7 +5,12 @@ RSpec.describe StringShuffle do
     expect(StringShuffle::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "shuffles a string" do
+    expect(StringShuffle.shuffle("hello")).to_not eq("hello")
   end
+
+  it "shuffles a string and removes new lines and white spaces" do
+    expect(StringShuffle.shuffle_remove_spaces(" h lo").size).to eq(3)
+  end
+
 end
